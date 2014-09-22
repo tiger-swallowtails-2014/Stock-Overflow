6149ab32af9f7d61f6f8acc85f8179423a427262
myeung58 <myeung58gmail.com>
2014-09-21 21:14:58 -0700

    fixed ajax and user page

---
+++ app/assets/javascripts/user_page.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('.question-form').hide();
+  $('.show-question-form').click(function() {
+    $('.question-form').toggle('slow');
+  })
+
+  $('.question-form').on("submit", function(event) {
+    event.preventDefault();
+    var request = $.ajax({url: $(this).attr("action"),
+                          type: "POST",
+                          data: $(this).serialize(),
+                          context: this
+    })
+    request.done(function(data) {
+      console.log("success");
+      location.reload();
+    })
+  })
+})
